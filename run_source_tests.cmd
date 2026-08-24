f4fbd9f475d8b94d1076eb74558943cabc743592
tuyenht <tuyenht@gmail.com>
2026-08-24 17:29:45 +0700

    test(ui): run S02-S09 evidence gate

--- run_source_tests.cmd
+++ run_source_tests.cmd
@@ -21,3 +21,5 @@ python tests\phase_a_ui_sanity.py
 if errorlevel 1 exit /b 1
 python tests\s01_s04_dynamic_binding_sanity.py
 if errorlevel 1 exit /b 1
+python tests\s02_s09_dynamic_binding_sanity.py
+if errorlevel 1 exit /b 1
